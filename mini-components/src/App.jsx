5a9fe62a63f2dd35c66c84e0ce1f0ccd14ecbf57
u53f <scalewithusef@gmail.com>
2022-10-26 18:59:12 +0100

    missing with components 💀

--- mini-components/src/App.jsx
+++ mini-components/src/App.jsx
@@ -1,3 +1,21 @@
 import "./App.css";
 
+// Components
+
+const btn = (bg = "tomato", color = "wheat") => {
+  const btnStyle = {
+    backgroundColor: bg,
+    color: color,
+  };
+  return (
+    <button className="btn" style={btnStyle}>
+      Click me
+    </button>
+  );
+};
+
+const App = () => {
+  return btn();
+};
+
 export default App;
