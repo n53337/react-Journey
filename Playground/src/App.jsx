20b7db9370cf26cde282bec1101d207fad216ca7
u53f <scalewithusef@gmail.com>
2022-10-27 15:31:16 +0100

    class based components 💥

--- Playground/src/App.jsx
+++ Playground/src/App.jsx
@@ -1,27 +1,31 @@
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import React from "react";
 
-// //!  Components and Props
-// const Name = (props) => {
-//   return (
-//     <p>
-//       Hey, I am {props.fn} {props.ln}, am {props.age}
-//     </p>
-//   );
-// };
+// //!  Functional Components and Props
+/*
+const Name = (props) => {
+  return (
+    <p>
+      Hey, I am {props.fn} {props.ln}, am {props.age}
+    </p>
+  );
+};
 
-// function App() {
-//   const name = {
-//     fn: "usef",
-//     ln: "u537",
-//     age: null,
-//   };
+function App() {
+  const name = {
+    fn: "usef",
+    ln: "u537",
+    age: null,
+  };
 
-//   return <Name {...name}></Name>;
-// }
+  return <Name {...name}></Name>;
+}
+*/
 
 // ! Destructring
 
+/*
 const Numbers = ({ numbers }) => {
   // modifying array to array of li JSX
   const list = numbers.map((number) => <li key={number}>{number}</li>);
@@ -43,6 +47,24 @@ const App = () => {
       </div>
     </div>
   );
+};
+*/
+
+// ! Components using Classes
+
+class Hi extends React.Component {
+  constructor(props) {
+    super(props);
+    this.name = props.name;
+  }
+
+  render() {
+    return <h1>Hey {this.name}</h1>;
+  }
+}
+
+const App = () => {
+  return <Hi name="usef"></Hi>;
 };
 
 export default App;
