7f912b4844baf85efe6286ddfd44fd923836925f
u53f <scalewithusef@gmail.com>
2022-10-27 10:51:20 +0100

    destructring props[200~const Numbers = ({ numbers }) => {

--- Playground/src/App.jsx
+++ Playground/src/App.jsx
@@ -1,23 +1,48 @@
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
-//  Components and Props
-const Name = (props) => {
-  return (
-    <p>
-      Hey, I am {props.fn} {props.ln}, am {props.age}
-    </p>
-  );
+// //!  Components and Props
+// const Name = (props) => {
+//   return (
+//     <p>
+//       Hey, I am {props.fn} {props.ln}, am {props.age}
+//     </p>
+//   );
+// };
+
+// function App() {
+//   const name = {
+//     fn: "usef",
+//     ln: "u537",
+//     age: null,
+//   };
+
+//   return <Name {...name}></Name>;
+// }
+
+// ! Destructring
+
+const Numbers = ({ numbers }) => {
+  // modifying array to array of li JSX
+  const list = numbers.map((number) => <li key={number}>{number}</li>);
+  return list;
 };
 
-function App() {
-  const name = {
-    fn: "usef",
-    ln: "u537",
-    age: null,
-  };
+// App component
 
-  return <Name {...name}></Name>;
-}
+const App = () => {
+  const numbers = [1, 2, 3, 4, 5];
+
+  return (
+    <div className="container">
+      <div>
+        <h1>Numbers List</h1>
+        <ul>
+          <Numbers numbers={numbers} />
+        </ul>
+      </div>
+    </div>
+  );
+};
 
 export default App;
