1f3dc9c8facef33468998b84b2c12caf8d7da975
u53f <scalewithusef@gmail.com>
2022-10-27 13:07:17 +0100

    population component

--- Projects/worldPopulation/src/App.jsx
+++ Projects/worldPopulation/src/App.jsx
@@ -1,6 +1,38 @@
 import { useState } from "react";
 import "./App.css";
+import coutries from "./countries";
+import countries from "./countries";
 
-const App = () => {};
+const Stats = ({ country }) => {
+  const chartSize = {
+    width: `${country.population / 250000000}rem`,
+  };
+  return (
+    <div className="country">
+      <div className="sec1">
+        <p>{country.country}</p>
+        <span style={chartSize}></span>
+      </div>
+      <p>{country.population}</p>
+    </div>
+  );
+};
 
+const Population = () => {
+  const renderCountry = countries.map((c, i) => (
+    <Stats country={c} key={`country${i}`}></Stats>
+  ));
+  return renderCountry;
+};
+
+const App = () => {
+  return (
+    <main>
+      <h1>World Population</h1>
+      <Population></Population>
+    </main>
+  );
+};
+
+// TODO On load Animation
 export default App;
